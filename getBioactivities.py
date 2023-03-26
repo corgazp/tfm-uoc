9db05755d5f59fecedfe806da8fb09e39f1bf498
corgaz <christianop_94@hotmail.com>
2023-03-26 11:41:21 +0200

    refactor

--- getBioactivities.py
+++ getBioactivities.py
@@ -9,12 +9,13 @@ import numpy as np
 from datetime import datetime
 # leemos el fichero y lo guardamos en la variable df
 df=pd.read_csv('file_with_cids.csv',sep=";")
-df_bioactivity=[]
+df_bioactivity=None
+arr_bioactivity=[]
 cids_with_error=[]
 cids_without_bioactivity=[]
 baseURL="https://pubchem.ncbi.nlm.nih.gov/sdq/sdqagent.cgi?infmt=json&outfmt=json&query={%22select%22:%22*%22,%22collection%22:%22bioactivity%22,%22where%22:{%22ands%22:[{%22cid%22:%22{cid}%22}]},%22start%22:{start},%22limit%22:10000}"
 
-def getBioactivitiesByCID(CID,start,limit,df_result):
+def getBioactivitiesByCID(CID,start,limit,arr_results,arr_errors, arr_no_bioactivity):
     response=None
     totalCount=0
     try:
@@ -23,27 +24,27 @@ def getBioactivitiesByCID(CID,start,limit,df_result):
             totalCount=response.json()["SDQOutputSet"][0]["totalCount"]
             start=start+limit
             if(totalCount>0):
-                df_result.append(pd.DataFrame(response.json()["SDQOutputSet"][0]["rows"]))
+                arr_results.append(pd.DataFrame(response.json()["SDQOutputSet"][0]["rows"]))
                 if(totalCount>start):
-                    getBioactivitiesByCID(CID,start,limit,df_result)
+                    getBioactivitiesByCID(CID,start,limit,arr_results, arr_errors, arr_no_bioactivity)
             else:
-                cids_without_bioactivity.append(CID)
+                arr_no_bioactivity.append(CID)
         else:
-            cids_with_error.append(CID)
+            arr_errors.append(CID)
     except:
-        cids_with_error.append(CID)
+        arr_errors.append(CID)
         if(response):
             print(f'API Resquest ERROR {response.status_code}')
         else:
             response="Error while execution"
             print(f'API Resquest ERROR: {response}')
 
-def getBioactivities(CIDs,start,limit,df_result):
+def getBioactivities(CIDs,start,limit,arr_results,arr_errors, arr_no_bioactivity):
     for i in CIDs:
-        getBioactivitiesByCID(i,start,limit,df_result)
-    pd.concat(df_result).dropna(subset="targetname").to_csv("bioactivities_by_cid.csv", sep=';',index=False)
+        getBioactivitiesByCID(i,start,limit,arr_results,arr_errors, arr_no_bioactivity)
+    resultsToCSV(arr_results)
     if(len(cids_with_error)>0):
-        getBioactivities(cids_with_error,start,limit,df_result)
+        getBioactivities(cids_with_error,start,limit,arr_results, arr_errors, arr_no_bioactivity)
 
 def checkbioactivity(CID, list_to_check):
     if(CID in list_to_check):
@@ -51,9 +52,16 @@ def checkbioactivity(CID, list_to_check):
     else:
         return "no"
 
+def resultsToCSV(arr_results):
+    pd.concat(arr_results).dropna(subset="targetname").to_csv("bioactivities_by_cid.csv", sep=';',index=False)
+
+def resultsToDataframe(arr_results):
+    return pd.concat(arr_results).dropna(subset="targetname")
+
 start_time = datetime.now()
-getBioactivities(df["cid"],1,1000,df_bioactivity)
-df["bioactivity"]=df.apply(lambda row:checkbioactivity(row["cid"], pd.unique(pd.concat(df_bioactivity).dropna(subset="targetname")["cid"])), axis = 1)
+getBioactivities(df["cid"],1,1000,arr_bioactivity,cids_with_error,cids_without_bioactivity)
+df_bioactivity=resultsToDataframe(arr_bioactivity)
+df["bioactivity"]=df.apply(lambda row:checkbioactivity(row["cid"], pd.unique(df_bioactivity["cid"])), axis = 1)
 df.to_csv("gut_comps_cids_bioactivity.csv", sep=';',index=False)
 end_time=datetime.now()
 print('Duration: {}'.format(end_time - start_time))
