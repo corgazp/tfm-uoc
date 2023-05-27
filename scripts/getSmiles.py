e794b5b61f30a2f434434bd96dd9c23d64cf4841
corgaz <christianop_94@hotmail.com>
2023-05-27 20:13:18 +0200

    descomentamos codigo

--- scripts/getSmiles.py
+++ scripts/getSmiles.py
@@ -7,7 +7,7 @@ import requests
 import numpy as np
 from datetime import datetime
 # leemos los ficheros y los guardamos en las variables df_guts y df_drugs
-# df_guts=pd.read_csv('../results/guts_cids.csv',sep=";")
+df_guts=pd.read_csv('../results/guts_cids.csv',sep=";")
 df_drugs=pd.read_csv('../results/drugs_cids.csv',sep=";")
 baseURL="https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{cid}/property/CanonicalSMILES/JSON"
 
@@ -41,16 +41,16 @@ def check_data_na(SMILES, cid):
 def generate_SEA_file(SMILES, CID, file):
     file.write(f"{SMILES} {CID}\n")
 
-# start_time = datetime.now()
-# df_guts["canonical_smiles"]=df_guts.apply(lambda row:get_smiles_by_cid(row["cid"]),axis=1)
-# df_guts["canonical_smiles"]=df_guts.apply(lambda row:check_data_na(row["canonical_smiles"], row["cid"]),axis=1)
-# df_guts.to_csv("../results/guts_smiles.csv", sep=';',index=False)
-# file_SEA=open("../results/input_guts.txt","w")
-# file_SEA.write("SMILES CID\n")
-# df_guts.apply(lambda row:generate_SEA_file(row["canonical_smiles"], row["cid"], file_SEA),axis=1)
-# file_SEA.close()
-# end_time=datetime.now()
-# print('Duration guts: {}'.format(end_time - start_time))
+start_time = datetime.now()
+df_guts["canonical_smiles"]=df_guts.apply(lambda row:get_smiles_by_cid(row["cid"]),axis=1)
+df_guts["canonical_smiles"]=df_guts.apply(lambda row:check_data_na(row["canonical_smiles"], row["cid"]),axis=1)
+df_guts.to_csv("../results/guts_smiles.csv", sep=';',index=False)
+file_SEA=open("../results/input_guts.txt","w")
+file_SEA.write("SMILES CID\n")
+df_guts.apply(lambda row:generate_SEA_file(row["canonical_smiles"], row["cid"], file_SEA),axis=1)
+file_SEA.close()
+end_time=datetime.now()
+print('Duration guts: {}'.format(end_time - start_time))
 start_time = datetime.now()
 df_drugs["canonical_smiles"]=df_drugs.apply(lambda row:get_smiles_by_cid(row["cid"]),axis=1)
 df_drugs["canonical_smiles"]=df_drugs.apply(lambda row:check_data_na(row["canonical_smiles"], row["cid"]),axis=1)
